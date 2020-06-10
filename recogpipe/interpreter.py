fed0ecb3fe2326afa2125be081844d6452162228
Mike C. Fletcher <mcfletch@vrplumber.com>
2020-06-10 00:47:41 -0400

    Start getting a reasonable selection of interpretations in

--- recogpipe/interpreter.py
+++ recogpipe/interpreter.py
@@ -16,18 +16,34 @@ def text_entry_rule(match, replace):
             result.append('^')
         result.append(text)
         if no_space_after:
-            replace.append('^')
+            result.append('^')
         return prefix + result + suffix
     apply_rule.__name__ = 'text_entry_%s'%("_".join(match))
     apply_rule.match = match 
+    apply_rule.text = text
     apply_rule.replace = replace
     apply_rule.no_space_after = no_space_after
     apply_rule.no_space_before = no_space_before
+    return apply_rule
 
+def transform_rule(match, transformation):
+    phrase = match[-1] == PHRASE_MARKER
+    word = match[-1] == WORD_MARKER
+    def apply_rule(words, start_index=0, end_index=-1):
+        """Given a match, transform and return the results"""
+        working = words[:]
+        if phrase:
+            working[start_index:] = transformation(words[end_index-1:])
+        else:
+            working[start_index:end_index] = transformation(words[end_index-1:end_index])
+        return working
+    apply_rule.__name__ = 'transform_entry_%s'%("_".join(match))
+    apply_rule.match = match 
+    apply_rule.text = None
     return apply_rule
 
 # General commands that already recognise well
-good_commands = """
+good_commands = '''
 # Typing characters
 # new line => ^'\\n'
 # new paragraph => ^'\\n'
@@ -35,14 +51,19 @@ period => ^'.'
 question mark => ^'?'
 exclamation mark => ^'!'
 open parentheses => '('^
+open parenthesis => '('^
 close parentheses => ^')'
+close parenthesis => ^')'
 underscore => ^'_'^
 open bracket => '['^
 close bracket => ^']'
 open quote => '"'^
 close quote => ^'"'
+quote quote => '""'
 open single quote => "'"^
 close single quote => ^"'"
+open triple quote => '"""'^
+close triple quote => ^'"""'
 ampersand character => "&"
 pipe character => "|"
 backslash => ^'\\'^
@@ -51,6 +72,8 @@ space => ^' '^
 bang => ^'!'
 at symbol => ^'@'^
 no space => ^''^
+dot => ^'.'^
+comma => ^','
 
 # # Key symbol entry
 # press enter => \\key (Enter)
@@ -66,20 +89,22 @@ no space => ^''^
 # escape => \\key (Escape)
 # alt ${word} => \\key (Alt+${word})
 
-# all caps on  => all_caps_start()
-# all caps off => all_caps_stop()
+all caps ${phrase}  => all_caps()
+title ${phrase} => title()
+cap ${word} => title()
+caps ${word} => title()
+capital ${word} => title()
+constant ${phrase} => constant()
+camel case ${phrase} => camel()
+
 # caps on => caps_on()
 # caps off => caps_off()
 # spell that => correction()
 # type that => correction()
-# cap ${word} => title(${word})
-# caps ${word} => title(${word})
-# capital ${word} => title(${word})
 # spell out => spell_on()
 # stop spell => spell_off()
 # spell stop =>  spell_off()
 # camel case => camel_on()
-# constant ${phrase} => camel_on() ${phrase} camel_off()
 # dunder ${phrase} => '_'^'_'^${phrase}^'_'^'_'
 
 # start listening => start()
@@ -95,7 +120,7 @@ no space => ^''^
 # select back ${number} words => \\key (Shift+Ctrl+Right)
 # select up line => \\key (Shift+Up)
 # select down line => \\key (Shift+Down)
-"""
+'''
 # General commands that don't recognise well
 BAD_COMMANDS = """
 press tab
@@ -111,6 +136,31 @@ octothorpe => ^'#'
 shebang => ^'#!'
 """
 
+NAMED_RULES = {}
+def named_rule(function):
+    NAMED_RULES[function.__name__] = function 
+    return function
+
+@named_rule
+def title(words):
+    return [word.title() for word in words]
+@named_rule 
+def all_caps(words):
+    return [word.upper() for word in words]
+@named_rule 
+def constant(words):
+    return ['^','_'.join([
+        x for x in all_caps(words)
+        if x != '^'
+    ]),'^']
+@named_rule 
+def camel(words):
+    return ['^',''.join([
+        x for x in title(words)
+        if x != '^'
+    ]),'^']
+
+
 def iter_rules(command_set):
     for i,line in enumerate(command_set.splitlines()):
         line = line.strip()
@@ -120,20 +170,23 @@ def iter_rules(command_set):
             pattern,target = line.split('=>',1)
         except ValueError as err:
             log.warning("Unable to parse rule #%i: %r", i+1, line)
-            import pdb;pdb.set_trace()
             continue
         pattern = pattern.strip().split()
         target = target.strip()
         yield pattern, target
 
-def load_commands(command_set,rules=None):
+def load_rules(command_set,rules=None):
     """load a set of commands from a string"""
     rules = rules or {}
     for pattern,target in iter_rules(command_set):
         branch = rules
         for word in pattern:
             branch = branch.setdefault(word,{})
-        branch[None] = text_entry_rule(pattern,target)
+        if target.endswith('()'):
+            rule = transform_rule(pattern,NAMED_RULES[target[:-2]])
+        else:
+            rule = text_entry_rule(pattern,target)
+        branch[None] = rule
     return rules
 
 # class Context(attr.s):
@@ -145,19 +198,55 @@ def load_commands(command_set,rules=None):
 #     def interpret(self, event):
 #         """Attempt to determine what this event likely means"""
 
+PHRASE_MARKER = '${phrase}'
+WORD_MARKER = '${word}'
+
 def match_rules(words,rules):
     """Find rules which match in the rules"""
     for start in range(len(words)):
-        # log.info("Start at %s", words[start:])
         branch = rules
+        i = 0
         for i,word in enumerate(words[start:]):
             if word in branch:
-                # log.info("%s in branch", word)
                 branch = branch[word]
+            elif WORD_MARKER in branch:
+                branch = branch[WORD_MARKER]
+            elif PHRASE_MARKER in branch:
+                branch = branch[PHRASE_MARKER]
+                break
             else:
                 # we don't match any further rules, do we 
                 # have a current match?
+                i -= 1
                 break
         if None in branch:
-            # log.info("Have none in branch")
-            return branch[None],words,start,start+i
+            rule = branch[None]
+            return rule,words,start,start+i+1
+
+def apply_rules(words, rules):
+    """Iteratively apply rules from rule-set until nothing changes"""
+    working = words[:]
+    for i in range(20):
+        match = match_rules(working,rules)
+        if match:
+            working = match[0](*match[1:])
+        else:
+            break
+    return working 
+
+def words_to_text(words):
+    """Compress words taking no-space markers into effect..."""
+    result = []
+    no_space = False
+    for item in words:
+        if item == '^':
+            no_space = True 
+        else:
+            if not no_space:
+                result.append(' ')
+            result.append(item)
+            no_space = False
+    if not no_space:
+        result.append(' ')
+    return ''.join(result)
+
