0f8135ad9134525dc4c3c68e1014f5f6954ecaa8
Mike C. Fletcher <mcfletch@vrplumber.com>
2020-06-26 14:33:05 -0400

    Start sketching in an out-of-process runner for audio

--- listener/qtgui/audioview.py
+++ listener/qtgui/audioview.py
@@ -1,11 +1,15 @@
 """Window configuring the audio input source"""
+import subprocess, time, logging, threading
 from PySide2.QtWidgets import QDockWidget, QPushButton, QVBoxLayout
-from ..resources import audiowindow
+from ..resources import listeneraudio
 from . import icons
-import subprocess
+from . import appref
+
+log = logging.getLogger(__name__)
 
 
 def describe_pulse_sources():
+    """Parse the output of pctl list to describe current pulse devices"""
     _interesting_headers = [
         'State',
         'Name',
@@ -39,13 +43,21 @@ def describe_pulse_sources():
     return sources
 
 
-class ListenerAudio(QDockWidget, audiowindow.Ui_ListenerAudio):
+class ListenerAudio(listeneraudio.Ui_ListenerAudio, QDockWidget):
     """Shows the Listener audio connection"""
+
+    app = property(appref.app)
+    GEOMETRY_SAVE_KEY = 'audioview.geometry'
+    INPUT_SAVE_KEY = 'audioview.microphone'
 
     def __init__(self, *args, **named):
         super(ListenerAudio, self).__init__(*args, **named)
+        self.want_input = True
         self.setupUi(self)
         self.set_available_inputs()
+        thread = threading.Thread(target=self.run_stream_thread,)
+        thread.setDaemon(True)
+        self.running_thread = thread
 
         # self.microphone_start = QPushButton(
         #     icons.get_icon('microphone-inactive'), 'Mic', self
@@ -65,4 +77,41 @@ class ListenerAudio(QDockWidget, audiowindow.Ui_ListenerAudio):
                 # don't really want to use output-monitor for dictation...
                 continue
             self.input_select.addItem(source['description'], source)
+
+            current = self.current_input()
+            index = self.input_select.findText(current,)
+            if index > -1:
+                self.input_select.setCurrentIndex(index)
+
+    def current_input(self):
+        current = self.app.settings.value(self.INPUT_SAVE_KEY,)
+        if current:
+            return current
+        return None
+
+    def run_stream_thread(self):
+        """Run our listener-audio stream in a subprocess"""
+        while self.want_input:
+            source = self.current_input()
+            command = [
+                'listener-audio',
+            ]
+            if source:
+                command += ['--device', source]
+            try:
+                pipe = subprocess.Popen(command)
+                try:
+                    while (
+                        pipe.poll() is None
+                        and self.want_input
+                        and source == self.current_input()
+                    ):
+                        time.sleep(1)
+                finally:
+                    if pipe.poll() is None:
+                        pipe.kill()
+
+            except Exception as err:
+                log.error("Failure during audio pipe setup")
+                time.sleep(2.0)
 
