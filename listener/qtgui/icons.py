a0af61c6db4df7ea7ec2e0d237a402c1c291db23
Mike C. Fletcher <mcfletch@vrplumber.com>
2020-07-19 18:30:22 -0400

    Pull icons from static location in all cases

--- listener/qtgui/icons.py
+++ listener/qtgui/icons.py
@@ -1,9 +1,12 @@
 """App-specific icons"""
 import os, logging, glob
 from PySide2 import QtGui
+from .. import defaults
+
+log = logging.getLogger(__name__)
 
 HERE = os.path.dirname(os.path.abspath((__file__)))
-ICON_DIR = os.path.normpath(os.path.join(HERE, '../static'))
+ICON_DIR = os.path.normpath(os.path.join(defaults.LISTENER_SOURCE, 'static'))
 
 ICONS = {
     # 'panel-icon-stopped': None,
@@ -21,7 +24,7 @@ def resource_icons():
     """Load our resource icons"""
     pattern = os.path.join(ICON_DIR, '*.svg')
     for name in glob.glob(pattern):
-        ICONS[os.path.basename(name[:-4])] = None
+        ICONS[os.path.basename(name[:-4])] = name
     assert 'microphone' in ICONS, (ICONS, pattern)
 
 
@@ -30,12 +33,17 @@ resource_icons()
 
 def get_icon(key):
     """Get our local/static icon by key/filename"""
+    # import pdb
+
+    # pdb.set_trace()
     current = ICONS.get(key)
     if current is None:
-        if key in ICONS:
-            current = ICONS[key] = QtGui.QIcon(os.path.join(ICON_DIR, key + '.svg'))
-        elif key != 'microphone':
+        if key != 'microphone':
             return QtGui.QIcon.fromTheme(key, get_icon('microphone'))
+    elif isinstance(current, str):
+        if key in ICONS:
+            log.warning("Loading %s from %s", key, current)
+            current = ICONS[key] = QtGui.QIcon(current)
         else:
             raise RuntimeError("We didn't load our default icon (microphone.svg)!")
     return current
